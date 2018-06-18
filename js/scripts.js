9917e396498d6d60f80f1a6b99db8eaf54dc1371
bunnymarie <bunnymarie@protonmail.com>
2018-06-17 20:34:35 -0700

    Resets total after order entry, displays items ordered.

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,57 @@
-function order(sidesOrderedReduced, beveragesOrderedReduced, dessertsOrderedReduced, pizzaReduced){
-  this.sidesOrderedReduced = sidesOrderedReduced;
-  this.beveragesOrderedReduced = beveragesOrderedReduced;
-  this.dessertsOrderedReduced = dessertsOrderedReduced;
-  this.pizzaReduced = pizzaReduced;
-}
-
-var sidesOrdered = [];
-var sidesOrderedReduced;
-var beveragesOrdered = [];
-var beveragesOrderedReduced;
-var dessertsListed = [];
-var dessertsOrdered = [];
-var dessertsOrderedReduced;
-
-
-var pizza = 10;
-var pizzaSauce = parseInt($("input:radio[name=pizzaSauce]:checked").val());
-var pizzaToppingsOrdered = [];
-var pizzaReduced;
-var total;
 
 $(document).ready(function() {
   $("form#orderForm").submit(function(event) {
     event.preventDefault();
-    debugger;
+
+    function order(sidesOrderedReduced, beveragesOrderedReduced, dessertsOrderedReduced, pizzaReduced){
+      this.sidesOrderedReduced = sidesOrderedReduced;
+      this.beveragesOrderedReduced = beveragesOrderedReduced;
+      this.dessertsOrderedReduced = dessertsOrderedReduced;
+      this.pizzaReduced = pizzaReduced;
+    }
+
+    var sidesListed = [];
+    var sidesOrdered = [];
+    sidesOrderedReduced = sidesOrderedReduced || 0
+    var sidesOrderedReduced;
+    var beveragesListed= [];
+    var beveragesOrdered = [];
+    var beveragesOrderedReduced;
+    beveragesOrderedReduced = beveragesOrderedReduced || 0
+    var dessertsListed = [];
+    var dessertsOrdered = [];
+    var dessertsOrderedReduced;
+    dessertsOrderedReduced = dessertsOrderedReduced || 0
+
+    var pizza = 10;
+    var pizzaSauce = parseInt($("input:radio[name=pizzaSauce]:checked").val());
+    var pizzaToppingsListed = [];
+    var pizzaToppingsOrdered = [];
+    var pizzaReduced;
+    pizzaReduced = pizzaReduced || 0
+    var total;
+
     var newOrder = new order(sidesOrderedReduced, beveragesOrderedReduced, dessertsOrderedReduced, pizzaReduced, total);
     order.prototype.total = function(){
       return beveragesOrderedReduced+sidesOrderedReduced+dessertsOrderedReduced+pizza+pizzaReduced;
     }
 
       $("input:checkbox[value=sides]:checked").each(function(){
+        sidesListed.push($(this).attr("id"));
         sidesOrdered.push(5);
         sidesOrderedReduced =  sidesOrdered.reduceRight(function(a,b){return a+b;});
         return sidesOrderedReduced;
       });
 
       $("input:checkbox[value=pizzaToppings]:checked").each(function(){
+        pizzaToppingsListed.push($(this).attr("id"));
         pizzaToppingsOrdered.push(1);
         pizzaReduced = pizzaToppingsOrdered.reduceRight(function(a,b){return a+b;});
         return pizzaReduced;
       });
 
       $("input:checkbox[value=beverages]:checked").each(function(){
+        beveragesListed.push($(this).attr("id"));
         beveragesOrdered.push(3);
         beveragesOrderedReduced = beveragesOrdered.reduceRight(function(a,b){return a+b;});
         return beveragesOrderedReduced;
@@ -54,17 +64,16 @@ $(document).ready(function() {
         return dessertsOrderedReduced;
       });
 
-      alert(beveragesOrderedReduced+sidesOrderedReduced+dessertsOrderedReduced);
-      alert(pizza+pizzaReduced);
-      alert(beveragesOrderedReduced+sidesOrderedReduced+dessertsOrderedReduced+pizza+pizzaReduced);
-
       $("ul#orders").append(
            "<li><span class='orderInfo'>Order1</span></li>");
          $(".orderInfo").last().click(function(){
            $("#show-info").show();
+           $(".sides").text(sidesListed);
+           $(".pizzaToppings").text(pizzaToppingsListed);
+           $(".beverages").text(beveragesListed);
            $(".desserts").text(dessertsListed);
            $(".total").text(newOrder.total);
-
          });
+
     });
 });
